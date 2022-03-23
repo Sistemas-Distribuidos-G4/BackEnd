3df82b3eb43d5cabf4d6d398bbbf96a751ebc1b1
Gustavo <lpc9730@gmail.com>
2022-03-23 11:16:45 -0500

    error

--- routes/patient.js
+++ routes/patient.js
@@ -5,7 +5,7 @@ const patientController = require('../controllers/patientController');
 
 // api/patients
 router.get('/', patientController.getPatients);
-router.put('/:id', patientController.updatePatient);
+// router.put('/:id', patientController.updatePatient);
 router.put('/:id', patientController.traupdPatient);
 router.post('/', patientController.createPatient);
 router.get('/', patientController.getPatients);
@@ -16,6 +16,5 @@ router.get('/patientsmatchm',patientController.patientsMatchM);
 router.get('/pacm', patientController.getPatientsM);
 router.get('/:id', patientController.getPatient);
 router.get('/dni/:dni', patientController.getPatientByDni);
-// router.put('/:id', patientController.updatePatient);
 router.delete('/:id', patientController.deletePatient);
 module.exports = router; 
